3d6b04abfb379f1a16e0d35d9b7094cacc4a937e
Steve Cha <aireat@gmail.com>
2016-09-22 12:03:10 +0900

    Add TestCase for "marcos"

---
+++ tests/test_macros.cpp
@@ -0,0 +1,23 @@
+
+
+#include "CppUTest/TestHarness.h"
+
+#include "count_lead_zero.h"
+
+
+TEST_GROUP(macros)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+    }
+};
+
+
+TEST(macros, normal)
+{
+
+}
