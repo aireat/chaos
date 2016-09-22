f916267d9a74b8d412203e7877e69d82e3c14475
Steve Cha <aireat@gmail.com>
2016-09-22 12:02:12 +0900

    Add TestCase for "Double Linked List"

---
+++ tests/test_double_linked_list.cpp
@@ -0,0 +1,251 @@
+
+
+#include "CppUTest/TestHarness.h"
+
+#include "double_linked_list.h"
+
+enum
+{
+    NODE_TEMP           = 0,
+    NODE_FIRST          ,
+    NODE_MID_1          ,
+    NODE_MID_2          ,
+    NODE_LAST           ,
+    NODE_COUNT          ,
+};
+
+TEST_GROUP(double_linked_list)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+    }
+};
+
+
+TEST(double_linked_list, init)
+{
+    DLIST_t     dlist = { (P_DNODE_t)1, };
+
+    dlist_init_list(&dlist);
+
+    LONGS_EQUAL(NULL, dlist.p_head);
+    LONGS_EQUAL(NULL, dlist.p_tail);
+}
+
+TEST(double_linked_list, empty_list_add_cut_node_at_head)
+{
+    DLIST_t     dlist;
+    DNODE_t     nodes[NODE_COUNT];
+
+    dlist_init_list(&dlist);
+
+    // add node at head
+    {
+        dlist_add_node_at_head(&dlist, &nodes[NODE_FIRST]);
+
+        LONGS_EQUAL(NULL, nodes[NODE_FIRST].p_next);
+        LONGS_EQUAL(NULL, nodes[NODE_FIRST].p_prev);
+
+        LONGS_EQUAL(&nodes[NODE_FIRST], dlist.p_head);
+        LONGS_EQUAL(&nodes[NODE_FIRST], dlist.p_tail);
+    }
+
+    // cut node
+    {
+        dlist_cut_node(&dlist, &nodes[NODE_FIRST]);
+
+        LONGS_EQUAL(NULL, nodes[NODE_FIRST].p_next);
+        LONGS_EQUAL(NULL, nodes[NODE_FIRST].p_prev);
+
+        LONGS_EQUAL(NULL, dlist.p_head);
+        LONGS_EQUAL(NULL, dlist.p_tail);
+    }
+}
+
+TEST(double_linked_list, empty_list_add_cut_node_at_tail)
+{
+    DLIST_t     dlist;
+    DNODE_t     nodes[NODE_COUNT];
+
+    dlist_init_list(&dlist);
+
+    // add node at tail
+    {
+        dlist_add_node_at_tail(&dlist, &nodes[NODE_LAST]);
+
+        LONGS_EQUAL(NULL, nodes[NODE_LAST].p_next);
+        LONGS_EQUAL(NULL, nodes[NODE_LAST].p_prev);
+
+        LONGS_EQUAL(&nodes[NODE_LAST], dlist.p_head);
+        LONGS_EQUAL(&nodes[NODE_LAST], dlist.p_tail);
+    }
+
+    // cut node
+    {
+        dlist_cut_node(&dlist, &nodes[NODE_LAST]);
+
+        LONGS_EQUAL(NULL, nodes[NODE_LAST].p_next);
+        LONGS_EQUAL(NULL, nodes[NODE_LAST].p_prev);
+
+        LONGS_EQUAL(NULL, dlist.p_head);
+        LONGS_EQUAL(NULL, dlist.p_tail);
+    }
+}
+
+TEST(double_linked_list, multi_add_node_at_head)
+{
+    INT         i;
+    DLIST_t     dlist;
+    DNODE_t     nodes[NODE_COUNT];
+
+    dlist_init_list(&dlist);
+
+    dlist_add_node_at_head(&dlist, &nodes[NODE_TEMP]);
+
+    // add node at head
+    for (i = NODE_FIRST; i < NODE_COUNT; i++)
+    {
+        dlist_add_node_at_head(&dlist, &nodes[i]);
+
+        LONGS_EQUAL(&nodes[i-1], nodes[i].p_next);
+        LONGS_EQUAL(NULL, nodes[i].p_prev);
+
+        LONGS_EQUAL(&nodes[i], dlist.p_head);
+        LONGS_EQUAL(&nodes[NODE_TEMP], dlist.p_tail);
+    }
+}
+
+TEST(double_linked_list, multi_add_node_at_tail)
+{
+    INT         i;
+    DLIST_t     dlist;
+    DNODE_t     nodes[NODE_COUNT];
+
+    dlist_init_list(&dlist);
+
+    dlist_add_node_at_tail(&dlist, &nodes[NODE_TEMP]);
+
+    // add node at head
+    for (i = NODE_FIRST; i < NODE_COUNT; i++)
+    {
+        dlist_add_node_at_tail(&dlist, &nodes[i]);
+
+        LONGS_EQUAL(NULL, nodes[i].p_next);
+        LONGS_EQUAL(&nodes[i-1], nodes[i].p_prev);
+
+        LONGS_EQUAL(&nodes[NODE_TEMP], dlist.p_head);
+        LONGS_EQUAL(&nodes[i], dlist.p_tail);
+    }
+}
+
+TEST(double_linked_list, multi_cut_node_at_head)
+{
+    INT         i;
+    DLIST_t     dlist;
+    DNODE_t     nodes[NODE_COUNT];
+
+    dlist_init_list(&dlist);
+
+    // add node at head
+    for (i = 0; i < NODE_COUNT; i++)
+    {
+        dlist_add_node_at_head(&dlist, &nodes[i]);
+    }
+
+    // cut node forward
+    for (i = NODE_COUNT-1; i > 0; i--)
+    {
+        dlist_cut_node(&dlist, &nodes[i]);
+
+        LONGS_EQUAL(NULL, nodes[i].p_next);
+        LONGS_EQUAL(NULL, nodes[i].p_prev);
+
+        LONGS_EQUAL(&nodes[i-1], dlist.p_head);
+        LONGS_EQUAL(&nodes[NODE_TEMP], dlist.p_tail);
+    }
+
+    // add node at head
+    for (i = 1; i < NODE_COUNT; i++)
+    {
+        dlist_add_node_at_head(&dlist, &nodes[i]);
+    }    
+
+    // cut node backward
+    for (i = 1; i < NODE_COUNT-1; i++)
+    {
+        dlist_cut_node(&dlist, &nodes[i]);
+
+        LONGS_EQUAL(NULL, nodes[i].p_next);
+        LONGS_EQUAL(NULL, nodes[i].p_prev);
+
+        LONGS_EQUAL(&nodes[NODE_COUNT-1], dlist.p_head);
+        LONGS_EQUAL(&nodes[NODE_TEMP], dlist.p_tail);
+    }
+
+    // cut last node except temp node
+    {
+        dlist_cut_node(&dlist, &nodes[NODE_COUNT-1]);    
+
+        LONGS_EQUAL(&nodes[NODE_TEMP], dlist.p_head);
+        LONGS_EQUAL(&nodes[NODE_TEMP], dlist.p_tail);
+    }
+}
+
+
+TEST(double_linked_list, multi_cut_node_at_tail)
+{
+    INT         i;
+    DLIST_t     dlist;
+    DNODE_t     nodes[NODE_COUNT];
+
+    dlist_init_list(&dlist);
+
+    // add node at head
+    for (i = 0; i < NODE_COUNT; i++)
+    {
+        dlist_add_node_at_tail(&dlist, &nodes[i]);
+    }
+
+    // cut node forward
+    for (i = 1; i < NODE_COUNT-1; i++)
+    {
+        dlist_cut_node(&dlist, &nodes[i]);
+
+        LONGS_EQUAL(NULL, nodes[i].p_next);
+        LONGS_EQUAL(NULL, nodes[i].p_prev);
+
+        LONGS_EQUAL(&nodes[NODE_TEMP], dlist.p_head);
+        LONGS_EQUAL(&nodes[NODE_COUNT-1], dlist.p_tail);
+    }
+
+    // cut last node except temp node
+    {
+        dlist_cut_node(&dlist, &nodes[NODE_COUNT-1]);    
+
+        LONGS_EQUAL(&nodes[NODE_TEMP], dlist.p_head);
+        LONGS_EQUAL(&nodes[NODE_TEMP], dlist.p_tail);
+    }
+
+    // add node at head
+    for (i = 1; i < NODE_COUNT; i++)
+    {
+        dlist_add_node_at_tail(&dlist, &nodes[i]);
+    }    
+
+    // cut node backward
+    for (i = NODE_COUNT-1; i > 0; i--)
+    {
+        dlist_cut_node(&dlist, &nodes[i]);
+
+        LONGS_EQUAL(NULL, nodes[i].p_next);
+        LONGS_EQUAL(NULL, nodes[i].p_prev);
+
+        LONGS_EQUAL(&nodes[NODE_TEMP], dlist.p_head);
+        LONGS_EQUAL(&nodes[i-1], dlist.p_tail);
+    }
+}
+
