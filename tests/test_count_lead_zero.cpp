712d6b84b3a6e0acc0714b1fa4c01c22bad2bc9f
Steve Cha <aireat@gmail.com>
2016-09-26 08:22:16 +0900

    Modify TestCase of "Count Lead Zero"

--- tests/test_count_lead_zero.cpp
+++ tests/test_count_lead_zero.cpp
@@ -17,34 +17,47 @@ TEST_GROUP(count_lead_zero)
 };
 
 
-TEST(count_lead_zero, normal)
+TEST(count_lead_zero, T01)
 {
     LONGS_EQUAL(32, count_lead_zero(0x00000000));
     LONGS_EQUAL( 0, count_lead_zero(0xFFFFFFFF));
     LONGS_EQUAL(31, count_lead_zero(0x00000001));
+}
 
+TEST(count_lead_zero, T02)
+{
     LONGS_EQUAL( 0, count_lead_zero(0x80000000));
     LONGS_EQUAL( 0, count_lead_zero(0xC0000000));
     LONGS_EQUAL( 0, count_lead_zero(0x81000000));
     LONGS_EQUAL( 0, count_lead_zero(0x80010001));
     LONGS_EQUAL( 0, count_lead_zero(0x80010101));
     LONGS_EQUAL( 0, count_lead_zero(0x80000001));
+}
 
+TEST(count_lead_zero, T03)
+{
     LONGS_EQUAL( 8, count_lead_zero(0x00800000));
     LONGS_EQUAL( 8, count_lead_zero(0x00C00000));
     LONGS_EQUAL( 8, count_lead_zero(0x00C00100));
     LONGS_EQUAL( 8, count_lead_zero(0x00801010));
     LONGS_EQUAL( 8, count_lead_zero(0x00800001));
+}
 
+TEST(count_lead_zero, T04)
+{
     LONGS_EQUAL(12, count_lead_zero(0x00080000));
     LONGS_EQUAL(12, count_lead_zero(0x000C0000));
     LONGS_EQUAL(12, count_lead_zero(0x000C0100));
     LONGS_EQUAL(12, count_lead_zero(0x00080101));
     LONGS_EQUAL(12, count_lead_zero(0x00080001));
+}
 
+TEST(count_lead_zero, T05)
+{
     LONGS_EQUAL(20, count_lead_zero(0x00000800));
     LONGS_EQUAL(20, count_lead_zero(0x00000C00));
     LONGS_EQUAL(20, count_lead_zero(0x00000C11));
     LONGS_EQUAL(20, count_lead_zero(0x00000811));
     LONGS_EQUAL(20, count_lead_zero(0x00000801));
 }
+
